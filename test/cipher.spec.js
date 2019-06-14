380982a6e0199e772331e65f6a49d57fc80aa0e9
yomiramontalvovelazco <ymontalvov@uni.pe>
2019-06-14 10:53:55 -0500

    Terminado1

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -13,10 +13,20 @@ describe('cipher', () => {
       assert.equal(cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 'HIJKLMNOPQRSTUVWXYZABCDEFG'); 
     });
     
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 10', () =>{
+    it('debería retornar "KLMNOPQRSTUVWXYZABCDEFGHIJ" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 10', () =>{
       assert.equal(cipher.encode(10, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 'KLMNOPQRSTUVWXYZABCDEFGHIJ'); 
     }); 
-    
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33npm test', () =>{
+      assert.equal(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz'), 'hijklmnopqrstuvwxyzabcdefg'); 
+    }); 
+    it('debería retornar "klmnopqrstuvwxyzabcdefghij" para "abcdefghijklmnopqrstuvwxyz" con offset 10', () =>{
+      assert.equal(cipher.encode(10, 'abcdefghijklmnopqrstuvwxyz'), 'klmnopqrstuvwxyzabcdefghij'); 
+    }); 
+    it('debería retornar " " para " " con offset 10', () =>{
+      assert.equal(cipher.encode(10, ' '), ' '); 
+    }); 
+
+
   });
 
   describe('cipher.decode', () => {
@@ -29,6 +39,15 @@ describe('cipher', () => {
     });
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "KLMNOPQRSTUVWXYZABCDEFGHIJ" con offset 10', () =>{
       assert.equal(cipher.decode(10, 'KLMNOPQRSTUVWXYZABCDEFGHIJ'), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'); 
+    }); 
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () =>{
+      assert.equal(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg'), 'abcdefghijklmnopqrstuvwxyz'); 
+    }); 
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "klmnopqrstuvwxyzabcdefghij" con offset 10', () =>{
+      assert.equal(cipher.decode(10, 'klmnopqrstuvwxyzabcdefghij'), 'abcdefghijklmnopqrstuvwxyz'); 
+    }); 
+    it('debería retornar " " para " " con offset 10', () =>{
+      assert.equal(cipher.decode(10, ' '), ' '); 
     }); 
 
 });
